16d50306df23a4aa897786386ade60b4683e8494
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2023-02-07 20:31:19 +0100

    Update Game.js

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -14,7 +14,29 @@ const Game = ({ rows, columns }) => {
   return (
     <div className="Game">
       {gameOver ? (
-        <Menu onClick={start} />
+        <div style={{    display: "flex",
+          flexDirection: "column",
+          alignItems: "center",}}>   
+          <Menu onClick={start} />
+          <div className="Instructions" style={{background: "rgb(100, 100, 100, 0.2)",
+    fontSize: "20px",
+    width: "300px",
+    display: "flex",
+    flexDirection: "column",
+    padding: "20px",
+    borderRadius: "20px",
+    position: "absolute",
+    bottom: "400px",}}>    
+            <p>KEYS:</p>
+            <p>arrow left: move left</p>
+            <p>arrow right: move right</p>
+            <p>arrow up: rotate</p>
+            <p>arrow down: slow drop</p>
+            <p>space bar: fast drop</p>
+            <p>q: quit</p>
+            <p>p: pause</p>
+          </div>
+        </div>
       ) : (
         <Tetris rows={rows} columns={columns} setGameOver={setGameOver} />
       )}
